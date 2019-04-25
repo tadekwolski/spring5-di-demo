45433b2bd750f31d5ba4b36e876d8d157a419650
Tadeusz Wolski <tadeusz.wolski@capgemini.com>
2019-04-25 20:40:43 +0200

    41 Spring Profiles

--- src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile("en")
+@Profile({"en","default"})
 public class PrimaryGreetingService implements GreetingService{
     @Override
     public String sayGreetings() {
